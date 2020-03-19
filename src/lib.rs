e7bf5753ef58a89485c9d9f6624543470a05d9a4
Lone-coder <abhijithcheruvery@gmail.com>
2020-03-19 10:21:44 +0530

    wrapped sdl2 for microengine

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,106 @@
+//Micro Engine core modules
 pub mod controller;
 pub mod window;
 pub mod input;
 pub mod world;
 pub mod game_object;
+pub mod camera;
+pub mod level;
+pub mod tile_object;
+
+// SDL2 crate
+extern crate sdl2;
+
+pub use sdl2::video::Window;
+pub use sdl2::event::Event;
+pub use sdl2::pixels::Color;
+pub use sdl2::keyboard::Keycode;
+pub use sdl2::rect::Rect;
+pub use std::collections::{HashMap,HashSet};
+pub use std::time::{Duration, Instant};
+
+pub struct Engine{
+    canvas : sdl2::render::Canvas<Window>,
+    event_pump : sdl2::EventPump,
+}
+
+impl Engine{
+
+    pub fn init_engine(screen_width : u32, screen_height : u32, window_title : &str) -> Engine
+    {
+        let sdl_context = sdl2::init().unwrap();
+
+    	let video_subsystem = sdl_context.video().unwrap();
+
+    	let window = video_subsystem.window(window_title, screen_width, screen_height)
+    	.position_centered().build().map_err(|e| e.to_string()).unwrap();
+
+        let mut _canvas = window.into_canvas()
+    	.accelerated().build().map_err(|e| e.to_string()).unwrap();
+
+        let _texture_creator = _canvas.texture_creator();
+
+        _canvas.set_draw_color(sdl2::pixels::Color::RGBA(0,0,0,255));
+
+        let timer = sdl_context.timer().unwrap();
+
+        let _event_pump = sdl_context.event_pump().unwrap();
+
+        Engine{
+            canvas : _canvas,
+            event_pump : _event_pump,
+        }
+
+    }
+
+    pub fn run( &mut self, update : fn(delta_time : f32),
+                handle_events : fn(event_pump : &mut sdl2::EventPump),
+                render : fn(canvas : &mut sdl2::render::Canvas<Window>) )
+    {
+        let mut running = true;
+    	let mut delta_time = 0.005;
+    	let mut min_fps = 10000.0;
+    	let mut max_fps = 0.0;
+
+        //main game loop
+    	while running {
+    		let start = Instant::now();
+
+            //event handling
+    	    for event in self.event_pump.poll_iter() {
+    	        match event {
+    	            Event::Quit {..} | Event::KeyDown {keycode: Some(Keycode::Escape), ..} => {
+    	                    running = false;
+    	                },
+    	            _=>(),
+    	        }
+    	    }
+
+            handle_events(&mut self.event_pump);
+            update(delta_time);
+
+            //rendering routine
+    		self.canvas.set_draw_color(Color::RGB(0, 0, 0));
+    		self.canvas.clear();
+    		render(&mut self.canvas);
+    		self.canvas.present();
+
+    	    //std::thread::sleep(std::time::Duration::from_millis(16)); //waiting for 60fps 1 /60 = 0.016 secs
+
+    		delta_time = start.elapsed().as_secs_f32();
+    		let fps = 1.0 / delta_time;
+    		//println!("{:?} fps , dt = {:?}", fps, delta_time);
+
+    		//catching min and max FPS
+    		if fps > max_fps {
+    			max_fps = fps;
+    		}
+
+    		if fps < min_fps{
+    			min_fps = fps;
+    		}
+        }
+
+    }
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,77 +1,25 @@
-extern crate sdl2;
-use sdl2::event::Event;
-use sdl2::pixels::Color;
-use sdl2::keyboard::Keycode;
-use sdl2::rect::Rect;
-use std::collections::{HashMap,HashSet};
-use std::time::{Duration, Instant}; //for calculationg delta time
+//default template for micro engine
+use micro_engine_rs;
 
-fn main(){
+fn handle_events(event_pump : &mut sdl2::EventPump)
+{
 
-	println!("testing");
-    let _out = main_loop();
 }
 
-fn main_loop()->Result<(),String>{
+fn update(delta_time :f32)
+{
 
-	let mut world = micro_engine_rs::world::World::generate(6,6);
+}
 
+fn render(canvas : &mut sdl2::render::Canvas<sdl2::video::Window>)
+{
+	canvas.set_draw_color(micro_engine_rs::Color::RGB(255, 210, 0));
+	canvas.fill_rect(micro_engine_rs::Rect::new(0, 0, 20,20)).unwrap();
+}
 
-	let sdl_context = sdl2::init()?;
-	let video_subsystem = sdl_context.video()?;
-	let window = video_subsystem.window("SDL2", 800, 600)
-	.position_centered().build().map_err(|e| e.to_string())?;
-	let mut canvas = window.into_canvas()
-	.accelerated().build().map_err(|e| e.to_string())?;
-	let texture_creator = canvas.texture_creator();
-	canvas.set_draw_color(sdl2::pixels::Color::RGBA(0,0,0,255));
-	let mut timer = sdl_context.timer()?;
-	let mut event_pump = sdl_context.event_pump()?;
+fn main()
+{
+	let mut engine = micro_engine_rs::Engine::init_engine(800, 600, "Micro engine test");
 
-	let mut running = true;
-	let mut delta_time = 0.005;
-	let mut min_fps = 10000.0;
-	let mut max_fps = 0.0;
-
-	while running {
-		let start = Instant::now();
-
-	    for event in event_pump.poll_iter() {
-	        match event {
-	            Event::Quit {..} | Event::KeyDown {keycode: Some(Keycode::Escape), ..} => {
-	                    running = false;
-	                },
-	            _=>(),
-	        }
-	    }
-
-		//rendering routine
-		canvas.set_draw_color(Color::RGB(0, 0, 0));
-		canvas.clear();
-		canvas.set_draw_color(Color::RGB(255, 210, 0));
-		world.render(&mut canvas);
-
-		canvas.present();
-
-		//world.test_display(
-	    //std::thread::sleep(std::time::Duration::from_millis(16)); //waiting for 60fps 1 /60 = 0.016 secs
-
-		delta_time = start.elapsed().as_secs_f32();
-		let fps = 1.0 / delta_time;
-		println!("{:?} fps , dt = {:?}", fps, delta_time);
-
-		//catching min and max FPS
-		if fps > max_fps {
-			max_fps = fps;
-		}
-
-		if fps < min_fps{
-			min_fps = fps;
-		}
-
-	}
-	//logging fps details
-	println!("min_fps = {:?}  , max_fps = {:?}", min_fps, max_fps);
-	Ok(())
-
+	engine.run(update, handle_events, render);
 }
