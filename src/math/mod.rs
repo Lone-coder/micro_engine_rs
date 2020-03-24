601cb609b6a262ee5be42661d431c348b4a45354
Lone-coder <abhijithcheruvery@gmail.com>
2020-03-24 19:14:21 +0530

    Added vector2

---
+++ src/math/mod.rs
@@ -0,0 +1,14 @@
+pub struct  Vector2 {
+    x : f32,
+    y : f32,
+}
+
+impl Vector2 {
+
+    pub fn new(_x : f32, _y :f32) -> Vector2 {
+        Vector2 {
+            x : _x,
+            y : _y,
+        }
+    }
+}
